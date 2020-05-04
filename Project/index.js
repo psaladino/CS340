67287c384a432f08fe580802f4b29a182a2bf8a0
frenchyPino <59627528+frenchyPino@users.noreply.github.com>
2020-05-04 12:19:16 -0700

    Project template

---
+++ Project/index.js
@@ -0,0 +1,44 @@
+var express = require('express');
+
+var path = require('path');
+var app = express();
+
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 3000);
+
+app.get('/', function(req, res) {
+    res.render('home');
+});
+
+app.get('/about', function(req, res) {
+    res.render('about');
+});
+
+app.get('/services', function(req, res) {
+    res.render('services');
+});
+
+app.get('/reviews', function(req, res) {
+    res.render('reviews');
+});
+
+app.use(function(req,res){
+    res.status(404);
+    res.render('404');
+});
+
+app.use(function(err, req, res, next){
+    console.error(err.stack);
+    res.type('plain/text');
+    res.status(500);
+    res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
