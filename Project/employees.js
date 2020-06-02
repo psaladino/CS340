43aa0faa1eb6a2bf0ce0157825cb76ad7bc6edfd
frenchyPino <59627528+frenchyPino@users.noreply.github.com>
2020-06-01 22:27:19 -0700

    update function

--- Project/employees.js
+++ Project/employees.js
@@ -22,8 +22,42 @@ module.exports = function(){
   
           //res.send('Here you go!');
       }
+
+      
         router.get('/', getEmployees);
 
+        router.get('/:id', function(req, res){
+            callbackCount = 0;
+            var context = {};
+            context.jsscripts = ["updateemployees.js"];
+            var mysql = req.app.get('mysql');
+            getEmployees(res, mysql, context, req.params.id, complete);
+            function complete(){
+                callbackCount++;
+                if(callbackCount >= 2){
+                    res.render('employees', context);
+                }
+    
+            }
+        });
+
+        router.put('/:id', function(req, res){
+            var mysql = req.app.get('mysql');
+            console.log(req.body)
+            console.log(req.params.id)
+            var sql = "UPDATE employees SET firstName=?, lastName=?, email=? WHERE userID=?";
+            var inserts = [req.body.fname, req.body.lname, req.body.homeworld, req.body.age, req.params.id];
+            sql = mysql.pool.query(sql,inserts,function(error, results, fields){
+                if(error){
+                    console.log(error)
+                    res.write(JSON.stringify(error));
+                    res.end();
+                }else{
+                    res.status(200);
+                    res.end();
+                }
+            });
+        });
     
     return router;
 }();
